829f307bd5c0dd5490d4924e550358bde5f94d24
manavdeora <147528604+manavdeora@users.noreply.github.com>
2024-04-05 21:13:14 +0530

    Add files via upload

---
+++ numberguessing.py
@@ -0,0 +1,20 @@
+def GuessTheNumber(Num):
+    no_of_chances = 10
+    i = 1
+    while i <= no_of_chances:
+        usr_inp = int(input("enter number here: "))
+        if usr_inp == Num:
+            print(f"Correct Guess,You Win!\nYou Take {i} chances to complete game.")
+            break
+        elif no_of_chances == i:    
+            print(f"Game Over,You Lose!")
+            i+=1
+        elif usr_inp > Num:
+            print(f"Too High and you have only {no_of_chances-i} chances")
+            i+=1
+        elif usr_inp < Num:
+            print(f"Too low and you have only {no_of_chances-i} chances")
+            i+=1 
+
+GuessTheNumber(12)
+
